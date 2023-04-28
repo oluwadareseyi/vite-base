806c5e632d13f059ff4ef7857bd4a780397e56f2
Oluwadare Seyi <oluwadareseyii@gmail.com>
2023-04-28 11:11:07 +0100

    chore: add map function

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,4 +1,13 @@
-import map from "lodash/map";
+const map = (array, iteratee) => {
+  let index = -1;
+  const length = array == null ? 0 : array.length;
+  const result = new Array(length);
+
+  while (++index < length) {
+    result[index] = iteratee(array[index], index, array);
+  }
+  return result;
+};
 
 export const findAncestor = (element, selector) => {
   while (
